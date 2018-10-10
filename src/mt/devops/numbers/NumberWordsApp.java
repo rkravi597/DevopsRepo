49af1cff76fc25b2d4f46eed430cfb032afa88a0
rkravi597 <rkravi597@gmail.com>
2018-10-10 17:29:01 +0530

    first commit

---
+++ src/mt/devops/numbers/NumberWordsApp.java
@@ -0,0 +1,38 @@
+package mt.devops.numbers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public final class NumberWordsApp {
+	private final NumberWords numberWords;
+	private final BufferedReader reader;
+
+	public NumberWordsApp() {
+		numberWords = new NumberWords();
+		reader = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	public void execute() {
+		while (true) {
+			try {
+				System.out.print("Input : 0 to stop the app: ");
+				String value = reader.readLine();
+				int number = Integer.parseInt(value);
+				String inWords = numberWords.toWords(number);
+				if (inWords.equals("0")) {
+					break;
+				} else {
+					System.out.println(inWords);
+				}
+			} catch (NumberFormatException | IOException e) {
+				System.out.println("Invalid number");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new NumberWordsApp().execute();
+	}
+
+}
